be2ec581539481346c2b35b9f10d90a67470b55d
k1dd <50468072+peter0x7f@users.noreply.github.com>
2024-05-16 16:01:26 -0400

    organize

--- note_bson_handling.py
+++
@@ -1,9 +0,0 @@
-import bson
-
-def serialize_data(data):
-    """Serializes Python data to BSON format."""
-    return bson.dumps(data)
-
-def deserialize_data(bson_data):
-    """Deserializes BSON data to Python format."""
-    return bson.loads(bson_data)
